dc95e8ca32f6b99787bf11a95f07a0fa7348e7c2
Graham Kennery <graham@kennery.com>
2014-03-05 09:55:43 -0500

    Documented indexeddbfs.js - and rewrote _getString to use _getBuffer and convert to a string only once

--- indexeddbfs.js
+++ indexeddbfs.js
@@ -1,3 +1,10 @@
+
+/**
+ * IndexedDBFs - a simple Javascript filesystem that allows you to store strings, blobs, arrays, array buffers and
+ * access them like long-term storage.
+ * @param {Object} options Different options for the filesystem
+ *                           - chunkSize: the number of bytes to segment each file into within the filesystem
+ */
 var IndexedDBFs = function (options) {
   options = options || {};
 
@@ -13,6 +20,10 @@ var IndexedDBFs = function (options) {
 }
 
 
+/**
+ * Returns a complete listing of filenames within the filesystem
+ * @param  {Function} cb Callback function (err, keys) {}
+ */
 IndexedDBFs.prototype.getFileList = function(cb) {
   this._fileList.keys(function(keys) {
     if (keys) {
@@ -23,12 +34,24 @@ IndexedDBFs.prototype.getFileList = function(cb) {
   });
 };
 
+
+/**
+ * Returns whether or not a file exists by a given name
+ * @param  {String}   fileName The file's name to look up
+ * @param  {Function} cb       Callback function (err, exists) {}
+ */
 IndexedDBFs.prototype.fileExists = function(fileName, cb) {
   this._fileList.exists(fileName, function(exists) {
     cb(null, exists);
   });
 };
 
+/**
+ * Creates a file and optionally sets metadata for the file
+ * @param  {String}   fileName The file's name to create
+ * @param  {Object}   data     The metadata to set on the file
+ * @param  {Function} cb       Callback function (err) {}
+ */
 IndexedDBFs.prototype.createFile = function(fileName, data, cb) {
   if (!cb && typeof data == 'function') {
     cb = data;
@@ -43,12 +66,24 @@ IndexedDBFs.prototype.createFile = function(fileName, data, cb) {
   });
 };
 
+
+/**
+ * Returns the metadata for a file
+ * @param  {String}   fileName the file's name to look up
+ * @param  {Function} cb       Callback function(err, fileObject) {}
+ */
 IndexedDBFs.prototype.getFileData = function(fileName, cb) {
   this._fileList.get(fileName, function(file) {
     cb(null, file);
   });
 };
 
+
+/**
+ * Deletes a file by its name
+ * @param  {String}   fileName The file name to delete
+ * @param  {Function} cb       Callback function(err) {}
+ */
 IndexedDBFs.prototype.deleteFile = function(fileName, cb) {
   this._fileList.remove(fileName, function() {
     cb && cb(null);
@@ -56,6 +91,11 @@ IndexedDBFs.prototype.deleteFile = function(fileName, cb) {
 };
 
 
+/**
+ * Deletes the entire filesystem - useful for testing
+ * @param  {String}   areYouSure A check value that must equal "yesplx" to run
+ * @param  {Function} cb         Callback function
+ */
 IndexedDBFs.prototype.nukeEverything = function(areYouSure, cb) {
   if (areYouSure === 'yesplx') {
     this._fileList.nuke();
@@ -65,6 +105,12 @@ IndexedDBFs.prototype.nukeEverything = function(areYouSure, cb) {
 };
 
 
+/**
+ * Gets a specific chunk from the database
+ * @param  {String}   fileName The file to get the chunk for
+ * @param  {Number}   chunkNum The chunk number to grab
+ * @param  {Function} cb       Callback function(err, ArrayBuffer)
+ */
 IndexedDBFs.prototype._getChunk = function(fileName, chunkNum, cb) {
   this._fileData.get(fileName + '_' + chunkNum, function(chunk) {
     cb(null, chunk ? chunk.chunk : null);
@@ -72,6 +118,13 @@ IndexedDBFs.prototype._getChunk = function(fileName, chunkNum, cb) {
 };
 
 
+/**
+ * Sets a specific chunk of a file
+ * @param {String}        fileName the file to set the chunk on
+ * @param {Number}        chunkNum The chunk number to set
+ * @param {ArrayBuffer}   chunk    The chunk data in ArrayBuffer format
+ * @param {Function}      cb       Callback function(err)
+ */
 IndexedDBFs.prototype._setChunk = function(fileName, chunkNum, chunk, cb) {
   var data = {
     key: fileName + '_' + chunkNum,
@@ -84,6 +137,15 @@ IndexedDBFs.prototype._setChunk = function(fileName, chunkNum, chunk, cb) {
 };
 
 
+/**
+ * Saves a file
+ * @param  {String}   fileName The filename
+ * @param  {Object}   fileData The data to save
+ *                               Current formats include: 
+ *                                 - String
+ *                                 - ArrayBuffer
+ * @param  {Function} cb       Callback function(err)
+ */
 IndexedDBFs.prototype.save = function(fileName, fileData, cb) {
   var toCall = null;
 
@@ -103,6 +165,14 @@ IndexedDBFs.prototype.save = function(fileName, fileData, cb) {
   }
 };
 
+
+/**
+ * Saves a file with string contents
+ *   Under the hood it converts the string into an ArrayBuffer and saves that.
+ * @param  {String}   fileName   The file name
+ * @param  {String}   stringData The string to save in the database
+ * @param  {Function} cb         Callback function(err)
+ */
 IndexedDBFs.prototype._saveString = function(fileName, stringData, cb) {
   var arrayBuffer = new ArrayBuffer(stringData.length * 2); // 2 bytes for each char
   var array = new Uint16Array(arrayBuffer);
@@ -113,6 +183,14 @@ IndexedDBFs.prototype._saveString = function(fileName, stringData, cb) {
   this._saveBuffer(fileName, arrayBuffer, cb);
 };
 
+
+/**
+ * Saves a file with ArrayBuffer contents
+ *   Under the hood it splits up the array buffer into smaller chunks and saves each one
+ * @param  {String}       fileName    The file name
+ * @param  {ArrayBuffer}  arrayBuffer The array buffer to save
+ * @param  {Function}     cb          Callback function(err)
+ */
 IndexedDBFs.prototype._saveBuffer = function(fileName, arrayBuffer, cb) {
   var self = this;
   var chunkNum = -1;
@@ -131,6 +209,12 @@ IndexedDBFs.prototype._saveBuffer = function(fileName, arrayBuffer, cb) {
 };
 
 
+/**
+ * Set the dataType for a file
+ * @param {String}   fileName The filename
+ * @param {String}   dataType The data type that the file is being stored as
+ * @param {Function} cb       Callback function(err)
+ */
 IndexedDBFs.prototype._setDataType = function(fileName, dataType, cb) {
   var self = this;
   this.getFileData(fileName, function(err, file) {
@@ -144,6 +228,11 @@ IndexedDBFs.prototype._setDataType = function(fileName, dataType, cb) {
 };
 
 
+/**
+ * Returns the dataType for a file
+ * @param  {String}   fileName The filename
+ * @param  {Function} cb       Callback function(err, dataType)
+ */
 IndexedDBFs.prototype._getDataType = function(fileName, cb) {
   this.getFileData(fileName, function(err, file) {
     if (!err && file) {
@@ -154,6 +243,12 @@ IndexedDBFs.prototype._getDataType = function(fileName, cb) {
   });
 };
 
+
+/**
+ * Returns a complete file in the format that it was saved in
+ * @param  {String}   fileName The filename
+ * @param  {Function} cb       Callback function(err, fileData)
+ */
 IndexedDBFs.prototype.getFile = function(fileName, cb) {
   var self = this;
   self._getDataType(fileName, function(err, dataType) {
@@ -173,29 +268,30 @@ IndexedDBFs.prototype.getFile = function(fileName, cb) {
   });
 };
 
+
+/**
+ * Returns a file that was stored in a string format
+ *   Retreives the chunks as an arrayBuffer and then converts it into a string before calling back
+ * @param  {String}   fileName The filename
+ * @param  {Function} cb       Callback function(err, fileData) {}
+ */
 IndexedDBFs.prototype._getString = function(fileName, cb) {
   var self = this;
   var chunkNum = -1;
-  var stringData = "";
-  function process() {
-    chunkNum++;
-    self._getChunk(fileName, chunkNum, function(err, chunk) {
-      if (!err) {
-        if (chunk) {
-          stringData += String.fromCharCode.apply(null, new Uint16Array(chunk));
-          process();
-        } else {
-          cb(null, stringData);
-        }
-      } else {
-        cb(err);
-      }
-    });
-  }
-
-  process();
+  this._getBuffer(fileName, function(err, buffer) {
+    if (!err && buffer) {
+      cb(null, String.fromCharCode.apply(null, new Uint16Array(buffer)));
+    } else {
+      cb(err || 'File not found');
+    }
+  });
 };
 
+/**
+ * Returns a file that was stored in an ArrayBuffer format
+ * @param  {String}   fileName The filename
+ * @param  {Function} cb       Callback function(err, fileData) {}
+ */
 IndexedDBFs.prototype._getBuffer = function(fileName, cb) {
   var self = this;
   var chunkNum = -1;
