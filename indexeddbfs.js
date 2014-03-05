62e3fe1eeea8b89ec5ed46aa77208af11f2065fd
Graham Kennery <graham@kennery.com>
2014-03-05 00:19:29 -0500

    SAVING AND GETTING STRINGS WOOOOO

--- indexeddbfs.js
+++ indexeddbfs.js
@@ -1,7 +1,7 @@
-var IndexedDBFs = function () {
-	// I need a fake filesystem in IndexedDB
-	this.maxChunkSize = 1024;
+var IndexedDBFs = function (options) {
+  options = options || {};
 
+	this.chunkSize = options.chunkSize || 1024;
 
   var self = this
   new Lawnchair({ adapter:'indexed-db', name: 'files', record: 'file' }, function() { 
@@ -43,7 +43,7 @@ IndexedDBFs.prototype.createFile = function(fileName, data, cb) {
   });
 };
 
-IndexedDBFs.prototype.getFile = function(fileName, cb) {
+IndexedDBFs.prototype.getFileData = function(fileName, cb) {
   this._fileList.get(fileName, function(file) {
     cb(null, file);
   });
@@ -62,4 +62,128 @@ IndexedDBFs.prototype.nukeEverything = function(areYouSure, cb) {
     this._fileData.nuke();
     setTimeout(cb, 100);
   }
+};
+
+
+IndexedDBFs.prototype._getChunk = function(fileName, chunkNum, cb) {
+  this._fileData.get(fileName + '_' + chunkNum, function(chunk) {
+    cb(null, chunk ? chunk.chunk : null);
+  });
+};
+
+
+IndexedDBFs.prototype._setChunk = function(fileName, chunkNum, chunk, cb) {
+  var data = {
+    key: fileName + '_' + chunkNum,
+    chunk: chunk
+  };
+
+  this._fileData.save(data, function() {
+    cb && cb(null);
+  })
+};
+
+
+IndexedDBFs.prototype.save = function(fileName, fileData, cb) {
+  if (typeof fileData === 'string') {
+    this._saveString(fileName, fileData, cb);
+  } else {
+    cb('Invalid data to save');
+  }
+};
+
+IndexedDBFs.prototype._saveString = function(fileName, stringData, cb) {
+  var arrayBuffer = new ArrayBuffer(stringData.length * 2); // 2 bytes for each char
+  var array = new Uint16Array(arrayBuffer);
+  for (var i = 0; i < stringData.length; i++) {
+    array[i] = stringData.charCodeAt(i);
+  }
+  
+  var self = this;
+  this._setDataType(fileName, 'string', function(err) {
+    if (err) {
+      self._saveBuffer(fileName, arrayBuffer, cb);
+    } else {
+      cb && cb(err);
+    }
+  });
+};
+
+IndexedDBFs.prototype._saveBuffer = function(fileName, arrayBuffer, cb) {
+  var self = this;
+  var chunkNum = -1;
+
+  function process() {
+    chunkNum++;
+    var chunk = arrayBuffer.slice(chunkNum * self.chunkSize, (chunkNum + 1) * self.chunkSize);
+    if (chunk.byteLength > 0) {
+      self._setChunk(fileName, chunkNum, chunk, process);
+    } else {
+      cb();
+    }
+  }
+
+  process();
+};
+
+
+IndexedDBFs.prototype._setDataType = function(fileName, dataType, cb) {
+  var self = this;
+  this.getFileData(fileName, function(err, file) {
+    if (!err && file) {
+      file.dataType = dataType;
+      self._fileList.save(file, cb);
+    } else {
+      cb(err || 'file not found');
+    }
+  });
+};
+
+
+IndexedDBFs.prototype._getDataType = function(fileName, cb) {
+  this.getFileData(fileName, function(err, file) {
+    if (!err && file) {
+      cb(null, file.dataType || null);
+    } else {
+      cb(err || 'file not found')
+    }
+  });
+};
+
+IndexedDBFs.prototype.getFile = function(fileName, cb) {
+  var self = this;
+  self._getDataType(fileName, function(err, dataType) {
+    if (!err) {
+      if (dataType === 'string') {
+        self._getString(fileName, cb);
+      } else {
+        cb('no data type - unable to process');
+      }
+    } else {
+      cb(err);
+    }
+  });
+};
+
+IndexedDBFs.prototype._getString = function(fileName, cb) {
+  var self = this;
+  var chunkNum = -1;
+  var stringData = "";
+  function process() {
+    chunkNum++;
+    self._getChunk(fileName, chunkNum, function(err, chunk) {
+      if (!err) {
+        if (chunk) {
+          stringData += String.fromCharCode.apply(null, new Uint16Array(chunk));
+          process();
+        } else {
+          cb(null, stringData);
+        }
+      } else {
+        cb(err);
+      }
+    });
+  }
+
+  process();
 };
